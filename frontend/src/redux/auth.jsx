d5d3a0ba61777d94b37f5b11e98ebcf0d061e10f
Andy <andy.lamyt@gmail.com>
2025-05-26 03:20:45 +0100

    fixed bugs

--- frontend/src/redux/auth.jsx
+++ frontend/src/redux/auth.jsx
@@ -1,9 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const baseUrl = import.meta.env.VITE_BASE_BACKEND_URL;
 const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "/auth",
+    baseUrl: `${baseUrl}/auth`,
   }),
   tagTypes: ["referesh"],
   endpoints: function (builder) {
